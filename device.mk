ebddc4f4b18e609e9c0f774233b2e405997794b8
Hecheng Yu <kde-yyds@qq.com>
2026-03-27 17:00:51 +0800

    avalon: Add support for OPlus Camera
    Change-Id: I6b26d49e072ac0dd69b8f9ab20f50b8c725022f8
    Signed-off-by: EternalMikaelson <klaus_mikaelson@rediffmail.com>

--- device.mk
+++ device.mk
@@ -29,6 +29,9 @@ PRODUCT_COPY_FILES += \
 # LiveDisplay
 $(call soong_config_set_bool,OPLUS_LINEAGE_LIVEDISPLAY_HAL,ENABLE_SE,false)
 
+# OPlus Camera
+$(call inherit-product-if-exists, vendor/oplus/camera/avalon/opluscamera.mk)
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay-lineage
