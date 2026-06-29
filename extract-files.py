b82e6b269298b6688553875a0d55bf312c33d96e
realahnet <syedmusicrealise@gmail.com>
2026-06-29 15:13:55 +0500

    avalon: Patch more libui dependant cam libs to depend on libui-stock

--- extract-files.py
+++ extract-files.py
@@ -72,7 +72,8 @@ blob_fixups: blob_fixups_user_type = {
         .clear_symbol_version('AHardwareBuffer_lock')
         .clear_symbol_version('AHardwareBuffer_lockPlanes')
         .clear_symbol_version('AHardwareBuffer_release')
-        .clear_symbol_version('AHardwareBuffer_unlock'),
+        .clear_symbol_version('AHardwareBuffer_unlock')
+        .replace_needed('libui.so', 'libui-stock.so'),
     'odm/lib64/libarcsoft_high_dynamic_range_v4.so': blob_fixup()
         .clear_symbol_version('remote_handle_close')
         .clear_symbol_version('remote_handle_invoke')
@@ -96,7 +97,11 @@ blob_fixups: blob_fixups_user_type = {
         .regex_replace('NFC_DEBUG_ENABLED=1', 'NFC_DEBUG_ENABLED=0'),
     'vendor/lib64/libcwb_qcom_aidl.so': blob_fixup()
         .add_needed('libui_shim.so'),
-    'odm/lib64/libsharebuffer_impl.so': blob_fixup()
+    (
+        'odm/lib64/camera/components/com.oplus.node.sstabphoto.so',
+        'odm/lib64/hw/camera.oemlayer.so',
+        'odm/lib64/libsharebuffer_impl.so',
+    ): blob_fixup()
         .replace_needed('libui.so', 'libui-stock.so'),
     'vendor/lib64/libui-stock.so': blob_fixup()
         .replace_needed('android.hardware.graphics.common-V4-ndk.so', 'android.hardware.graphics.common-V7-ndk.so'),
